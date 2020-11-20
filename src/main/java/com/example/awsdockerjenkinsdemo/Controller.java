b4aa4b1387ea72a4015f0ab2847ec691866274af
FrederikKea <fred1952@stud.kea.dk>
2020-11-20 14:51:38 +0100

    TEST1

--- src/main/java/com/example/awsdockerjenkinsdemo/Controller.java
+++ src/main/java/com/example/awsdockerjenkinsdemo/Controller.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @org.springframework.stereotype.Controller
 public class Controller {
 
+    //TEST1
+
     @GetMapping("/")
     public String index (){
         return "index";
