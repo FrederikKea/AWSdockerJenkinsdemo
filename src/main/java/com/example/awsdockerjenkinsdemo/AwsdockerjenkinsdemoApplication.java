3b552f43708e0e8cc7c523001dafc7452fe61d45
FrederikKea <fred1952@stud.kea.dk>
2020-11-20 13:50:55 +0100

    simpel hallo world springboot webapp to AWS docker jenkins

---
+++ src/main/java/com/example/awsdockerjenkinsdemo/AwsdockerjenkinsdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.awsdockerjenkinsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsdockerjenkinsdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsdockerjenkinsdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/awsdockerjenkinsdemo/AwsdockerjenkinsdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awsdockerjenkinsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsdockerjenkinsdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
